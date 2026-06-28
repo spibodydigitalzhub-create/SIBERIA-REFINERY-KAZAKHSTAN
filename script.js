06ca09ab3f8a0c17145c260863c7d13f0c953950
Spibody Digitalz Hub <spibodydigitalzhub@gmail.com>
2026-06-28 20:52:00 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// MOBILE MENU
-
-const menuBtn = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
-
-menuBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
